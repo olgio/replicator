eb5cf74bd16d9a05362e5b1f58c5033ab85a5741
Smirnov Oleg <itech7@yandex.ru>
2021-04-01 18:52:54 +0300

    Логирование сообщений в TypedActor

--- replicator-api/src/main/kotlin/ru/splite/replicator/transport/TypedActor.kt
+++ replicator-api/src/main/kotlin/ru/splite/replicator/transport/TypedActor.kt
@@ -3,6 +3,8 @@ package ru.splite.replicator.transport
 import kotlinx.serialization.BinaryFormat
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.protobuf.ProtoBuf
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import ru.splite.replicator.bus.NodeIdentifier
 
 abstract class TypedActor<T>(
@@ -17,16 +19,30 @@ abstract class TypedActor<T>(
     }
 
     suspend fun send(dst: NodeIdentifier, payload: T): T {
-        val requestBytes = binaryFormat.encodeToByteArray(this.serializer, payload)
-        val responseBytes = transport.send(this, dst, requestBytes)
-        return binaryFormat.decodeFromByteArray(this.serializer, responseBytes)
+        val start = System.currentTimeMillis()
+
+        val requestBytes: ByteArray = binaryFormat.encodeToByteArray(this.serializer, payload)
+        val responseBytes: ByteArray = transport.send(this, dst, requestBytes)
+        val response = binaryFormat.decodeFromByteArray(this.serializer, responseBytes)
+
+        LOGGER.debug("Sent message to $dst in ${System.currentTimeMillis() - start} ms: $payload ==> $response")
+        return response
     }
 
     abstract suspend fun receive(src: NodeIdentifier, payload: T): T
 
     override suspend fun receive(src: NodeIdentifier, payload: ByteArray): ByteArray {
-        val requestBytes = binaryFormat.decodeFromByteArray(this.serializer, payload)
-        val responseBytes = receive(src, requestBytes)
-        return binaryFormat.encodeToByteArray(this.serializer, responseBytes)
+        val start = System.currentTimeMillis()
+
+        val request: T = binaryFormat.decodeFromByteArray(this.serializer, payload)
+        val response: T = receive(src, request)
+        val responseBytes: ByteArray = binaryFormat.encodeToByteArray(this.serializer, response)
+
+        LOGGER.debug("Handled message from $src in ${System.currentTimeMillis() - start} ms: $request ==> $response")
+        return responseBytes
+    }
+
+    companion object {
+        private val LOGGER: Logger = LoggerFactory.getLogger(javaClass.enclosingClass)
     }
 }
