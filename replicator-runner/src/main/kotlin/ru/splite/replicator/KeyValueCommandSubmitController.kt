ed7c8c9b7289d255a8f98d46831c7f0c0a42f6f6
Smirnov Oleg <itech7@yandex.ru>
2021-04-01 18:57:17 +0300

    Базовая реализация демоп-приложения Runner для KV на базе Atlas

---
+++ replicator-runner/src/main/kotlin/ru/splite/replicator/KeyValueCommandSubmitController.kt
@@ -0,0 +1,76 @@
+package ru.splite.replicator
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import ru.splite.replicator.keyvalue.KeyValueCommand
+import ru.splite.replicator.keyvalue.KeyValueReply
+import ru.splite.replicator.statemachine.StateMachineCommandSubmitter
+
+class KeyValueCommandSubmitController(
+    private val port: Int,
+    private val config: AtlasProtocolConfig,
+    private val stateMachineCommandSubmitter: StateMachineCommandSubmitter<ByteArray, ByteArray>
+) {
+
+    fun start() {
+        embeddedServer(Netty, port = port) {
+
+            install(StatusPages) {
+                exception<Throwable> { cause ->
+                    call.respond(HttpStatusCode.InternalServerError, cause.toString())
+                    LOGGER.error("Error while handling request", cause)
+                }
+            }
+
+            install(DefaultHeaders) {
+                header("Node-Identifier", config.address.identifier)
+            }
+
+            routing {
+                route("/kv") {
+                    get("{key}") {
+                        val key = call.parameters["key"] ?: error("Key must be specified")
+                        val command = KeyValueCommand.GetValue(key)
+                        LOGGER.debug("Received GET command $command")
+                        val commandReply = submitCommand(command)
+                        call.respondText(Json.encodeToString(commandReply), ContentType.Application.Json)
+                    }
+
+                    post("{key}") {
+                        val key = call.parameters["key"] ?: error("Key must be specified")
+                        val value = call.receiveText()
+                        val command = KeyValueCommand.PutValue(key, value)
+                        LOGGER.debug("Received PUT command $command")
+                        val commandReply = submitCommand(command)
+                        call.respondText(Json.encodeToString(commandReply), ContentType.Application.Json)
+                    }
+                }
+
+                get("health") {
+                    call.respondText("OK")
+                }
+            }
+        }.start(wait = false)
+    }
+
+    private suspend fun submitCommand(command: KeyValueCommand): KeyValueReply {
+        val start = System.currentTimeMillis()
+        val resultBytes = stateMachineCommandSubmitter.submit(KeyValueCommand.serialize(command))
+        LOGGER.debug("Submitted command $command in ${System.currentTimeMillis() - start} ms")
+        return KeyValueReply.deserializer(resultBytes)
+    }
+
+    companion object {
+        val LOGGER: Logger = LoggerFactory.getLogger(javaClass.enclosingClass)
+    }
+}
