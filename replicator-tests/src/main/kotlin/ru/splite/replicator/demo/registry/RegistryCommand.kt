7b44ab064b345f9fce3e7e3bee91f5deecd9a840
Smirnov Oleg <itech7@yandex.ru>
2021-05-14 14:35:49 +0300

    Удалена неиспользуемая реализация RegistryStateMachine

--- replicator-tests/src/main/kotlin/ru/splite/replicator/demo/registry/RegistryCommand.kt
+++
@@ -1,8 +0,0 @@
-package ru.splite.replicator.demo.registry
-
-sealed class RegistryCommand {
-
-    data class PutValue(val value: Long) : RegistryCommand()
-
-    data class IncValue(val delta: Long) : RegistryCommand()
-}
